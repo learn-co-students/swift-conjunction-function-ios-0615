38b159aeb2fe693ec0918c969a48810c5a11367a
markedwardmurray <markedwardmurray@gmail.com>
2015-10-30 15:49:10 -0400

    setup Sandbox and SandboxSpec

---
+++ swift-conjunction-functionTests/SandboxSpec.swift
@@ -0,0 +1,18 @@
+//  SandboxSpec.swift
+
+import Quick
+import Nimble
+@testable import swift_conjunction_function
+
+class SandboxSpec: QuickSpec {
+    override func spec() {
+        describe("Sandbox") {
+            describe("") {
+                it("") {
+                    
+                }
+            }
+        }
+    }
+}
+
